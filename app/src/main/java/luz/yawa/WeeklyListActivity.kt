de749f6a1ca0e94d837d200ba41dca4faaca5a4e
AndreLuz <th3lightman@gmail.com>
2016-11-25 14:43:44 +0000

    lista dos proximos dias a funcionar, só falta os icones

--- app/src/main/java/luz/yawa/WeeklyListActivity.kt
+++ app/src/main/java/luz/yawa/WeeklyListActivity.kt
@@ -1,8 +1,10 @@
 package luz.yawa
 
 import android.app.ListActivity
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,7 +15,9 @@ import android.widget.Toast
 import com.android.volley.toolbox.JsonObjectRequest
 import org.json.JSONArray
 import org.json.JSONObject
+import java.text.DateFormat
 import java.text.DecimalFormat
+import java.text.SimpleDateFormat
 import java.util.*
 
 class WeeklyListActivity : ListActivity() {
@@ -22,21 +26,22 @@ class WeeklyListActivity : ListActivity() {
     private val KEY = "&APPID=f3f9bd7053ff66839aae078ee1214109"
     private val ICON_URL = "http://openweathermap.org/img/w/"
     private val ICON_EXTENSION = ".png"
-    private var list : List<Map<String, Any>>? = null
-    private var LANG_URI = "&lang="
-    private var TEMP_UNIT = "&units="
+    private var list: List<Map<String, Any>>? = null
+    private val LANG_URI = "&lang="
+    private val TEMP_UNIT = "&units="
 
-    data class WeatherInfo(val icon:String, val description :String,
-                           val max_temp :Double, val min_temp :Double)
+    data class WeatherInfo(val icon: String, val description: String,
+                           val max_temp: Double?, val min_temp: Double?)
 
-    class WeatherDaysHolder(val iconView :ImageView, val descView :TextView,
-                            val maxTempView :TextView, val minTempView :TextView)
+    class WeatherDaysHolder(val iconView: ImageView, val descView: TextView,
+                            val maxTempView: TextView, val minTempView: TextView)
 
-    private var theDaysInfo :Array<WeatherInfo>? = null
+    private var theDaysInfo: Array<WeatherInfo>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weekly_list)
+
         //adicionar a uma classe Utils
         operator fun JSONArray.iterator() =
                 (0 until length()).asSequence().map { idx -> get(idx) as JSONObject }.iterator()
@@ -47,18 +52,67 @@ class WeeklyListActivity : ListActivity() {
                         null,
                         {
                             val days_list = it.get("list") as JSONArray
-                            val days_info = days_list.iterator().asSequence().map {
+                            /*val days_info = days_list.iterator().asSequence().map {
                                 WeatherInfo((it.get("weather") as JSONArray).getJSONObject(0).get("icon") as String,
                                         (it.get("weather") as JSONArray).getJSONObject(0).get("description") as String,
-                                        (it.get("main") as JSONObject).get("temp_max") as Double,
-                                        (it.get("main") as JSONObject).get("temp_min") as Double)
-                            }.toList().toTypedArray()
+                                        (it.get("main") as JSONObject).get("temp") as Double,
+                                        (it.get("dt") as Int))
+                            }.toList().toTypedArray()*/
 
-                            theDaysInfo = days_info
+                            val days_seq = days_list.iterator().asSequence()
+
+                            val time_today = GregorianCalendar()
+                            val time_day = GregorianCalendar()
+                            time_day.add(GregorianCalendar.DATE, 1)
+
+                            val days: ArrayList<WeatherInfo> = ArrayList<WeatherInfo>()
+                            val date_format: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.getDefault())
+                            var date_converted: Date
+
+                            val time_aux = GregorianCalendar()
+
+                            var max_temp: Double? = null
+                            var min_temp: Double? = null
+                            val temp_icon: String? = null
+                            val temp_desc: String? = null
+
+                            for(day in days_list){
+                                val day_date = day.get("dt_txt") as String
+                                date_converted = date_format.parse(day_date)
+                                time_aux.time = date_converted
+
+                                //remove possible weather predictions from today
+                                if(time_aux.get(GregorianCalendar.DAY_OF_MONTH) == time_today.get(GregorianCalendar.DAY_OF_MONTH))
+                                    continue
+
+                                val temp = (day.get("main") as JSONObject).get("temp") as Double
+
+                                if(time_aux.get(GregorianCalendar.DAY_OF_MONTH) == time_day.get(GregorianCalendar.DAY_OF_MONTH)){
+                                    temp_icon ?: (day.get("weather") as JSONArray).getJSONObject(0).get("icon") as String
+                                    temp_desc ?: (day.get("weather") as JSONArray).getJSONObject(0).get("description") as String
+
+                                    if(max_temp == null || min_temp == null) {
+                                        max_temp = temp
+                                        min_temp = temp
+                                    }
+                                    else{
+                                        if(temp > max_temp) max_temp = temp
+                                        else if(temp < min_temp) min_temp = temp
+                                    }
+                                }
+                                else{
+                                    time_day.add(GregorianCalendar.DATE, 1)
+                                    days.add(WeatherInfo((day.get("weather") as JSONArray).getJSONObject(0).get("icon") as String,
+                                            (day.get("weather") as JSONArray).getJSONObject(0).get("description") as String,
+                                            max_temp,
+                                            min_temp))
+                                }
+                            }
+                            val days_arr = days.toTypedArray()
 
                             val list_layout = R.layout.list_day_info
                             val inflater = LayoutInflater.from(this)
-                            val adapter = object : ArrayAdapter<WeatherInfo>(this, list_layout, days_info){
+                            val adapter = object : ArrayAdapter<WeatherInfo>(this, list_layout, days_arr){
                                 override fun getView(position: Int, convertView :View?, parent : ViewGroup?) : View {
                                     val item_view = convertView ?:
                                             inflater.inflate(list_layout, parent, false)
@@ -72,13 +126,13 @@ class WeeklyListActivity : ListActivity() {
                                                         )
                                                     })*/
 
-                                    var view_holder = WeatherDaysHolder(
+                                    val view_holder = WeatherDaysHolder(
                                             item_view.findViewById(R.id.list_weather_icon) as ImageView,
                                             item_view.findViewById(R.id.list_weather_info) as TextView,
                                             item_view.findViewById(R.id.list_max_temp) as TextView,
                                             item_view.findViewById(R.id.list_min_temp) as TextView
                                     )
-                                    val day_weather = days_info[position]
+                                    val day_weather = days_arr[position]
 
                                     val d_format : DecimalFormat = DecimalFormat("#")
                                     d_format.isDecimalSeparatorAlwaysShown = false
